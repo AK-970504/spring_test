5282ee3416e5312b0165e8672bd8413f735ddc86
晴山 大樹 <ak.970504.ko@gmail.com>
2025-11-23 05:16:00 +0900

    2511230515 Commit

--- spring_test/src/main/java/jp/co/sss/spring_test/controller/PurchaseDetailController.java
+++ spring_test/src/main/java/jp/co/sss/spring_test/controller/PurchaseDetailController.java
@@ -1,5 +1,7 @@
 package jp.co.sss.spring_test.controller;
 
+import java.time.LocalDateTime;
+
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jp.co.sss.spring_test.entity.Carts;
+import jp.co.sss.spring_test.entity.Products;
 import jp.co.sss.spring_test.entity.Users;
+import jp.co.sss.spring_test.repository.CartsRepository;
+import jp.co.sss.spring_test.repository.ProductDetailRepository;
 import jp.co.sss.spring_test.repository.UserRepository;
 
 @Controller
 public class PurchaseDetailController {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private ProductDetailRepository productDetailRepository;
+	@Autowired
+	private CartsRepository cartsRepository;
 	@GetMapping("/purchase/purchaseDetail")
 	public String showPurchaseDetail() {
 		return "purchase/purchaseDetail";
@@ -43,5 +54,29 @@ public class PurchaseDetailController {
 		userRepository.save(loginUser);
 		session.setAttribute("loginUser", loginUser);
 		return "redirect:/purchase/purchaseCompletion";
+	}
+	@PostMapping("/purchase/single")
+	public String singlePurchase(
+		@RequestParam("product_id") Integer productId,
+		HttpSession session,
+		RedirectAttributes redirectAttributes) {
+		Users loginUser = (Users) session.getAttribute("loginUser");
+		if (loginUser == null) {
+			redirectAttributes.addFlashAttribute("errorMessage", "ログインが必要です");
+			return "redirect:/user/userLogin";
+		}
+		Products product = productDetailRepository.findById(productId).orElse(null);
+		if (product == null) {
+			redirectAttributes.addFlashAttribute("errorMessage", "商品が見つかりません");
+			return "redirect:/product/list";
+		}
+		Carts cart = new Carts();
+		cart.setUser(loginUser);
+		cart.setProduct(product);
+		cart.setQuantity(1);
+		cart.setCreated_at(LocalDateTime.now());
+		cart.setUpdated_at(LocalDateTime.now());
+		cartsRepository.save(cart);
+		return "redirect:/purchase/purchaseDetail?cartId=" + cart.getCart_id();
 	}
 }
