e9447fec62f1854f6b31b912dbeafed89c4559db
晴山 大樹 <ak.970504.ko@gmail.com>
2025-11-27 10:41:41 +0900

    2511271041 Commit

--- spring_test/src/main/java/jp/co/sss/spring_test/controller/ProductDetailController.java
+++ spring_test/src/main/java/jp/co/sss/spring_test/controller/ProductDetailController.java
@@ -1,16 +1,26 @@
 package jp.co.sss.spring_test.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jp.co.sss.spring_test.entity.Carts;
 import jp.co.sss.spring_test.entity.Products;
 import jp.co.sss.spring_test.entity.Reviews;
+import jp.co.sss.spring_test.entity.Users;
+import jp.co.sss.spring_test.repository.CartsRepository;
+import jp.co.sss.spring_test.repository.ProductDetailRepository;
 import jp.co.sss.spring_test.service.ProductDetailService;
 import jp.co.sss.spring_test.service.ReviewService;
 
@@ -20,6 +30,10 @@ public class ProductDetailController {
 	private ProductDetailService productDetailService;
 	@Autowired
 	private ReviewService reviewService;
+	@Autowired
+	private ProductDetailRepository productDetailRepository;
+	@Autowired
+	private CartsRepository cartsRepository;
 	//商品詳細画面表示
 	@GetMapping("/product/productDetail/{id}")
 	public String showProductDetail(@PathVariable("id") Integer id, Model model) {
@@ -40,5 +54,29 @@ public class ProductDetailController {
 			 return product.getImg_path();
 		}
 		return null;
+	}
+	@PostMapping("/purchase/purchaseDetail")
+	public String singlePurchase(
+		@RequestParam("product_id") Integer productId,
+		HttpSession session,
+		RedirectAttributes redirectAttributes) {
+		Users loginUser = (Users) session.getAttribute("loginUser");
+		if (loginUser == null) {
+			redirectAttributes.addFlashAttribute("errorMessage", "ログインが必要です");
+			return "redirect:/user/userLogin";
+		}
+		Products product = productDetailRepository.findById(productId).orElse(null);
+		if (product == null) {
+			redirectAttributes.addFlashAttribute("errorMessage", "商品が見つかりません");
+			return "redirect:/product/list";
+		}
+		Carts cart = new Carts();
+		cart.setUser(loginUser);
+		cart.setProduct(product);
+		cart.setQuantity(1);
+		cart.setCreated_at(LocalDateTime.now());
+		cart.setUpdated_at(LocalDateTime.now());
+		cartsRepository.save(cart);
+		return "redirect:/purchase/purchaseDetail/" + cart.getCart_id();
 	}
 }
--- spring_test/src/main/java/jp/co/sss/spring_test/controller/PurchaseDetailController.java
+++ spring_test/src/main/java/jp/co/sss/spring_test/controller/PurchaseDetailController.java
@@ -1,33 +1,26 @@
 package jp.co.sss.spring_test.controller;
 
-import java.time.LocalDateTime;
-
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jp.co.sss.spring_test.entity.Carts;
-import jp.co.sss.spring_test.entity.Products;
 import jp.co.sss.spring_test.entity.Users;
-import jp.co.sss.spring_test.repository.CartsRepository;
-import jp.co.sss.spring_test.repository.ProductDetailRepository;
 import jp.co.sss.spring_test.repository.UserRepository;
 
 @Controller
 public class PurchaseDetailController {
 	@Autowired
 	private UserRepository userRepository;
-	@Autowired
-	private ProductDetailRepository productDetailRepository;
-	@Autowired
-	private CartsRepository cartsRepository;
-	@GetMapping("/purchase/purchaseDetail")
-	public String showPurchaseDetail() {
+	@GetMapping("/purchase/purchaseDetail/{cartId}")
+	public String showPurchaseDetail(
+		@PathVariable("cartId") Integer cartId,
+		Model model) {
 		return "purchase/purchaseDetail";
 	}
 	@PostMapping("/purchase/saveAddress")
@@ -54,29 +47,5 @@ public class PurchaseDetailController {
 		userRepository.save(loginUser);
 		session.setAttribute("loginUser", loginUser);
 		return "redirect:/purchase/purchaseCompletion";
-	}
-	@PostMapping("/purchase/single")
-	public String singlePurchase(
-		@RequestParam("product_id") Integer productId,
-		HttpSession session,
-		RedirectAttributes redirectAttributes) {
-		Users loginUser = (Users) session.getAttribute("loginUser");
-		if (loginUser == null) {
-			redirectAttributes.addFlashAttribute("errorMessage", "ログインが必要です");
-			return "redirect:/user/userLogin";
-		}
-		Products product = productDetailRepository.findById(productId).orElse(null);
-		if (product == null) {
-			redirectAttributes.addFlashAttribute("errorMessage", "商品が見つかりません");
-			return "redirect:/product/list";
-		}
-		Carts cart = new Carts();
-		cart.setUser(loginUser);
-		cart.setProduct(product);
-		cart.setQuantity(1);
-		cart.setCreated_at(LocalDateTime.now());
-		cart.setUpdated_at(LocalDateTime.now());
-		cartsRepository.save(cart);
-		return "redirect:/purchase/purchaseDetail?cartId=" + cart.getCart_id();
 	}
 }
